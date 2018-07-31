70b8f19025e9054f19401de2f326d301bb34b52b
Stephen Craton <stephen@tubig.me>
2018-07-30 21:42:30 -0500

    Implement filtering on session play state changes.

--- nodes/playing.js
+++ nodes/playing.js
@@ -5,6 +5,9 @@ module.exports = function(RED) {
     constructor(config) {
       RED.nodes.createNode(this, config);
 
+      this.filters = config.filters || [];
+      this.filters.sort((a, b) => a.idx > b.idx ? 1 : a.idx < b.idx ? -1 : 0);
+
       this.server = RED.nodes.getNode(config.server);
       utils.bindStateHandlers(this, this.server);
 
@@ -20,6 +23,53 @@ module.exports = function(RED) {
       return !!this.server ? this.server.plex : null;
     }
 
+    matchesFilters(session) {
+        let match = true;
+
+        this.filters.forEach((filter) => {
+            let sessionValue = session;
+            let filterValue = filter.value;
+            let comparison;
+
+            const keyParts = filter.key.split('.');
+            keyParts.forEach((key) => {
+                sessionValue = sessionValue[key];
+            });
+
+            switch (filter.valueType) {
+                case 'str':
+                    sessionValue = String(sessionValue);
+                    filterValue = String(filterValue);
+                    break;
+
+                case 'num':
+                    sessionValue = Number(sessionValue);
+                    filterValue = Number(filterValue);
+                    break;
+
+                case 'bool':
+                    sessionValue = Boolean(sessionValue);
+                    filterValue = Boolean(filterValue);
+                    break;
+            }
+
+            switch (filter.operator) {
+                case 'eq':  comparison = filterValue == sessionValue; break;
+                case 'neq': comparison = filterValue != sessionValue; break;
+                case 'lt':  comparison = filterValue < sessionValue; break;
+                case 'lte': comparison = filterValue <= sessionValue; break;
+                case 'gt':  comparison = filterValue > sessionValue; break;
+                case 'gte': comparison = filterValue >= sessionValue; break;
+            }
+
+            console.log(`evaluate ${filterValue} ${filter.operator} ${sessionValue}`);
+
+            match = match && comparison;
+        });
+
+        return match;
+    }
+
     begin() {
       this.plex.on('playing', (state, notification) => {
         const sessions = this.server.sessions;
@@ -32,8 +82,11 @@ module.exports = function(RED) {
                 session: session
             };
 
-            if (session['prevState'] !== state) {
-                this.send(msg);
+            if (session && session['prevState'] !== state) {
+                if (this.matchesFilters(session)) {
+                    this.send(msg);
+                }
+
                 session['prevState'] = state;
             }
 
